0b788ad8a6c1cb6d74d92602597319f2d3c7252b
Francesco_Penasa <francesco.penasa-1@studenti.unitn.it>
2018-10-24 10:54:41 +0200

    non funziona ma ci sono quasi

--- client.js
+++ client.js
@@ -7,17 +7,35 @@ var city = readline.question("What is your city,state?");
 const KEY = "TEk64IAYCLwiOukbMn3UC878UMWRHxJD";
 const url = "http://www.mapquestapi.com/geocoding/v1/address?key="+KEY+"&location="+city;
 
+const getLocationSun = async urlsun => {
+	try {
+	  const response = await fetch(url);
+	  const jsonb = await response.json();
+	  console.log("SUNRISE:")
+	  console.log(jsonb.results)
+	  console.log("SUNSET:")
+	  console.log(jsonb.status)
+	} catch (error) {
+	  console.log(error);
+	}
+  };
+
 const getLocation = async url => {
   try {
     const response = await fetch(url);
 	const json = await response.json();
-	console.log("json.result = ")
-	console.log(json.results);
 
-	console.log("json.FUNZIONAT")
-	console.log(json.results[0].locations[0].latLng.lng);
-	console.log("JSON")
-    console.log(json)
+	var lng = json.results[0].locations[0].latLng.lng;
+	var lat = json.results[0].locations[0].latLng.lat;
+
+
+	var urlsun = "https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lng;
+	console.log(urlsun);	
+	getLocationSun(urlsun);
+	
+	
+	
+
   } catch (error) {
 	  console.log("vai vai")
     console.log(error);
