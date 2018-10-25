e99b6a9228820597ca807c8375f889ffbe33c304
francesco <francesco.penasa-1@studenti.unitn.it>
2018-10-25 14:58:23 +0200

    refactor of client.js

--- client.js
+++ client.js
@@ -1,34 +1,49 @@
-const fetch = require("node-fetch");
-
-var readline = require('readline-sync');
-var city = readline.question("What is your city,state?");
-
+const fetch = require('node-fetch');
 const KEY = "TEk64IAYCLwiOukbMn3UC878UMWRHxJD";
-const url = "http://www.mapquestapi.com/geocoding/v1/address?key="+KEY+"&location="+city;
+const url_mapquest = "http://www.mapquestapi.com/geocoding/v1/address?key="+KEY+"&location="+process.argv[2]+","+process.argv[3];
 
-
-const getLocation = async url => {
+const getSunrisetime = async url_mapquest => {
   try {
-    const response = await fetch(url);
-	const json = await response.json();
+    const response = await fetch(url_mapquest);
+		const json_mapquest = await response.json();
 
-	var lng = json.results[0].locations[0].latLng.lng;
-	var lat = json.results[0].locations[0].latLng.lat;
-	
-	var url2 = "https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lng;
-	const response2 = await fetch(url2);
-	const json2 = await response2.json();
-	
-	console.log("Sunrise of "+city+ " is: \n");
-	console.log(json2);
+		var lng = json_mapquest.results[0].locations[0].latLng.lng;
+		var lat = json_mapquest.results[0].locations[0].latLng.lat;
+
+		const url_sunrise = "https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lng;
+
+		const res_sunrise = await fetch(url_sunrise);
+		const json_sunrise = await res_sunrise.json();
+		
+		console.log("Sunrise of "+process.argv[2]+","+process.argv[3]+" is: \n");
+		console.log(json_sunrise);
 
   } catch (error) {
-	  console.log("vai vai")
     console.log(error);
   }
 };
 
+getSunrisetime(url_mapquest);
 
+
+
+
+
+
+/** 
+
+
+const url = "http://localhost:3000/";
+
+const getLocation = async url => {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json)
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 getLocation(url);
-
+*/
