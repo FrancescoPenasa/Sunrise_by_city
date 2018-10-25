cb5e006eb902b005a04f9c46a5368b25b8578651
Francesco_Penasa <francesco.penasa-1@studenti.unitn.it>
2018-10-25 09:12:46 +0200

    funziona

--- client.js
+++ client.js
@@ -3,22 +3,9 @@ const fetch = require("node-fetch");
 var readline = require('readline-sync');
 var city = readline.question("What is your city,state?");
 
-
 const KEY = "TEk64IAYCLwiOukbMn3UC878UMWRHxJD";
 const url = "http://www.mapquestapi.com/geocoding/v1/address?key="+KEY+"&location="+city;
 
-const getLocationSun = async urlsun => {
-	try {
-	  const response = await fetch(url);
-	  const jsonb = await response.json();
-	  console.log("SUNRISE:")
-	  console.log(jsonb.results)
-	  console.log("SUNSET:")
-	  console.log(jsonb.status)
-	} catch (error) {
-	  console.log(error);
-	}
-  };
 
 const getLocation = async url => {
   try {
@@ -27,14 +14,13 @@ const getLocation = async url => {
 
 	var lng = json.results[0].locations[0].latLng.lng;
 	var lat = json.results[0].locations[0].latLng.lat;
-
-
-	var urlsun = "https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lng;
-	console.log(urlsun);	
-	getLocationSun(urlsun);
 	
+	var url2 = "https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lng;
+	const response2 = await fetch(url2);
+	const json2 = await response2.json();
 	
-	
+	console.log("Sunrise of "+city+ " is: \n");
+	console.log(json2);
 
   } catch (error) {
 	  console.log("vai vai")
@@ -43,8 +29,6 @@ const getLocation = async url => {
 };
 
 
+
 getLocation(url);
 
-
-//var lat = results.0.locations.0.latLng.lat;
-//var lng = results.0.locations.0.latLng.lng;
